64bbf7341d162bc4571a7f73730fe753c0ee9799
Norman Jäckel <openslides-protocol@normanjaeckel.de>
2014-06-05 12:18:09 +0200

    Updated metadata for upgrade to OpenSlides 1.6.

--- openslides_protocol/__init__.py
+++ openslides_protocol/__init__.py
@@ -4,13 +4,12 @@ from inspect import stack
 
 for frame in stack():
     lines = frame[4]
-    if lines and 'Nesizeecofujeiyeu1ahreiNgeiqu5kiocheitho' in lines[0]:
+    if lines and 'Peikee0iuv7uhikuashotohch6eec6ohseuNg7su' in lines[0]:
         break
 else:
-    from . import signals  # noqa
+    from . import main_menu, signals  # noqa
     from .urls import urlpatterns  # noqa
-    from .main_menu import ProtocolMainMenuEntry  # noqa
 
-__verbose_name__ = 'Protocol Plugin for OpenSlides'
-__description__ = 'This plugin provides a protocol function for each agenda item in OpenSlides.'
+__verbose_name__ = 'OpenSlides Protocol Plugin'
+__description__ = 'This plugin for OpenSlides provides a protocol of events managed in OpenSlides.'
 __version__ = '1.0-dev'
--- setup.py
+++ setup.py
@@ -2,39 +2,37 @@
 
 from setuptools import find_packages, setup
 
+package_name = 'openslides-protocol'
 module_name = 'openslides_protocol'
 
 # The following commented unique string is used to detect this import.
-module = __import__(module_name)  # HNesizeecofujeiyeu1ahreiNgeiqu5kiocheitho
+module = __import__(module_name)  # Peikee0iuv7uhikuashotohch6eec6ohseuNg7su
 
 with open('README.rst') as readme:
     long_description = readme.read()
 
-with open('requirements_production.txt') as requirements_production:
-    install_requires = requirements_production.readlines()
+with open('requirements.txt') as requirements:
+    install_requires = requirements.readlines()
 
 setup(
-    name='openslides-protocol',
+    name=package_name,
     version=module.__version__,
     description=module.__verbose_name__,
     long_description=long_description,
-    author='OpenSlides team, see AUTHORS',
+    author='Authors of %s, see AUTHORS' % module.__verbose_name__,
     author_email='support@openslides.org',
-    url='https://github.com/OpenSlides/openslides-protocol',
+    url='http://openslides.org/',
     keywords='OpenSlides',
-    classifiers = [
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Intended Audience :: Other Audience',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-    ],
+        'Programming Language :: Python :: 2'],
     license='MIT',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=install_requires,
     entry_points={'openslides_plugins': '%s = %s' % (module.__verbose_name__, module_name)})
-
